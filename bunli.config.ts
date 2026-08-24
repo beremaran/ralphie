3f0f6184b0db938a3ebc2221e6ef68bdf3991d2a
Berke Arslan <berke@kwilabs.com>
2026-08-24 08:32:10 +0800

    fuck bunli

--- bunli.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@bunli/core";
-
-export default defineConfig({
-    name: "ralphie",
-    version: "0.1.0",
-    commands: {
-        directory: "./src/commands",
-    },
-});
